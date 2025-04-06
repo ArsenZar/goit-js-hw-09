83d877bf0b2e83fcbed506a93ea67a5c021bdd9f
ArsenZar <arsenzar@gmail.com>
2025-04-06 14:42:29 +0300

    create data in localStorage and check them in appliacation

--- js/task-1.js
+++ js/task-1.js
@@ -1,3 +1,31 @@
 localStorage.setItem("ui-theme", "light");
 
+
 console.log(localStorage);
+
+const settings = {
+  theme: "dark",
+  isAuthenticated: true,
+  options: [1, 2, 3],
+};
+
+localStorage.setItem("settings", JSON.stringify(settings));
+/*
+const form = document.querySelector(".feedback-form");
+const textarea = form.elements.message;
+const localStorageKey = "goit-example-message";
+
+textarea.value = localStorage.getItem(localStorageKey) ?? "";
+
+form.addEventListener("input", (evt) => {
+  localStorage.setItem(localStorageKey, evt.target.value);
+});
+
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+	console.log(evt.target.elements.message.value);
+  localStorage.removeItem(localStorageKey);
+  form.reset();
+});
+
+*/
